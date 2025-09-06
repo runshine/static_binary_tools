8a762371cae2cb7e5f0e0548828abbbbebf41e83
runshine <woshiyuhao0819@gmail.com>
2025-09-07 00:23:43 +0800

    add binutils build

--- build/binutils/build.sh
+++ build/binutils/build.sh
@@ -5,7 +5,7 @@ source "$(cd `dirname $0`;pwd)/../common/prepare_dir.sh"
 VERSION="2.45"
 apt-get -y install curl ca-certificates wget xz-utils libssl-dev libncurses-dev autoconf git libwrap0-dev libreadline-dev zstd libz-dev
 
-curl -o "${SOURCE_DIR}/binutils-${VERSION}.tar.zst" "https://ftp.gnu.org/gnu/binutils/binutils-${VERSION}.tar.zst"
+curl -o "${SOURCE_DIR}/binutils-${VERSION}.tar.zst" "https://ftp.wayne.edu/gnu/binutils/binutils-${VERSION}.tar.zst"
 [ -d "${BUILD_DIR}/binutils-${VERSION}" ] && rm -rf "${BUILD_DIR}/binutils-${VERSION}"
 cd "${BUILD_DIR}/" && tar -xf "${SOURCE_DIR}/binutils-${VERSION}.tar.zst"
 rm -f "${SOURCE_DIR}/binutils-${VERSION}.tar.zst"
