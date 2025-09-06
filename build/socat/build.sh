84aa60695a1f229ff7425a13c65e2bb8490ad922
runshine <woshiyuhao0819@gmail.com>
2025-09-06 23:32:11 +0800

    add socat build

--- build/socat/build.sh
+++ build/socat/build.sh
@@ -10,7 +10,7 @@ git clone http://repo.or.cz/socat.git
 cd "${SOURCE_DIR}/socat"
 git clean -fdx
 autoconf
-LDFLAGS="-static" ./configure --prefix="${INSTALL_DIR}"
+LDFLAGS="-static" ./configure --prefix="${INSTALL_DIR}" --enable-openssl-base --enable-openssl-method --enable-resolve --enable-fips
 make -j 4
 strip -s "socat"
 mkdir -p "${INSTALL_DIR}/bin" && mv "socat" "${INSTALL_DIR}/bin/socat-linux-$(uname -m)"
