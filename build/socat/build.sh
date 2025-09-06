0f845f294366cdc8785e497dd926eef532bf8514
runshine <woshiyuhao0819@gmail.com>
2025-09-06 23:31:36 +0800

    add socat build

---
+++ build/socat/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source "$(cd `dirname $0`;pwd)/../common/prepare_dir.sh"
+
+apt-get -y install curl ca-certificates wget xz-utils libssl-dev libncurses-dev autoconf git libwrap0-dev libreadline-dev
+
+cd "${SOURCE_DIR}"
+git clone http://repo.or.cz/socat.git
+
+cd "${SOURCE_DIR}/socat"
+git clean -fdx
+autoconf
+LDFLAGS="-static" ./configure --prefix="${INSTALL_DIR}"
+make -j 4
+strip -s "socat"
+mkdir -p "${INSTALL_DIR}/bin" && mv "socat" "${INSTALL_DIR}/bin/socat-linux-$(uname -m)"
+
+
