ca361f2f75cc41a91d787dbdc9b87ff90d712a94
runshine <447491275@qq.com>
2025-12-12 13:59:05 +0000

    fix armel

--- build/common/arch_detect.sh
+++ build/common/arch_detect.sh
@@ -3,7 +3,7 @@
 ARCH="$(uname -m)"
 
 if [ "$ARCH" = "armv7l" ];then
-  if [ "x${cat /proc/self/maps|grep gnueabihf}" != "x" ];then
+  if [ "x$(cat /proc/self/maps|grep gnueabihf)" != "x" ];then
     ARCH="armhf"
   else
     ARCH="armel"
