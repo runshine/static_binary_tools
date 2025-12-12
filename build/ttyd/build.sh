ca012807fa089d60d9559c7e0e62a17ff2b3104a
runshine <447491275@qq.com>
2025-12-12 13:28:40 +0000

    add ttyd

--- build/ttyd/build.sh
+++ build/ttyd/build.sh
@@ -11,26 +11,29 @@ TTYD_ARMHF="https://github.com/tsl0922/ttyd/releases/download/${VERSION}/ttyd.ar
 TTYD_ARMEL="https://github.com/tsl0922/ttyd/releases/download/${VERSION}/ttyd.arm"
 TTYD_X64="https://github.com/tsl0922/ttyd/releases/download/${VERSION}/ttyd.x86_64"
 
-mkdir -p "$INSTALL_DIR/bin"
 
+mkdir -p "$INSTALL_DIR/bin"
 cd "$INSTALL_DIR"
 curl -o "$INSTALL_DIR/bin/ttyd" "${TTYD_AARCH64}"
 strip_elf_files "$INSTALL_DIR/bin"
 package_release_tar "${INSTALL_DIR}" ttyd-linux-aarch64.tar.gz
 mv ttyd-linux-aarch64.tar.gz "$INSTALL_DIR/../"
 
+mkdir -p "$INSTALL_DIR/bin"
 cd "$INSTALL_DIR"
 curl -o "$INSTALL_DIR/bin/ttyd" "${TTYD_ARMHF}"
 strip_elf_files "$INSTALL_DIR/bin"
 package_release_tar "${INSTALL_DIR}" ttyd-linux-armhf.tar.gz
 mv ttyd-linux-armhf.tar.gz "$INSTALL_DIR/../"
 
+mkdir -p "$INSTALL_DIR/bin"
 cd "$INSTALL_DIR"
 curl -o "$INSTALL_DIR/bin/ttyd" "${TTYD_ARMEL}"
 strip_elf_files "$INSTALL_DIR/bin"
 package_release_tar "${INSTALL_DIR}" ttyd-linux-armel.tar.gz
 mv ttyd-linux-armel.tar.gz "$INSTALL_DIR/../"
 
+mkdir -p "$INSTALL_DIR/bin"
 cd "$INSTALL_DIR"
 curl -o "$INSTALL_DIR/bin/ttyd" "${TTYD_X64}"
 strip_elf_files "$INSTALL_DIR/bin"
