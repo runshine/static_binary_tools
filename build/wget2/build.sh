3669c296df846f3f5a1595e658feac26c0f3a9c1
runshine <447491275@qq.com>
2025-12-13 09:48:14 +0000

    add wget2 binary

--- build/wget2/build.sh
+++ build/wget2/build.sh
@@ -9,7 +9,7 @@ PACKAGE="wget2"
 EXT="tar.gz"
 apt-get -y install curl ca-certificates wget xz-utils libssl-dev libncurses-dev autoconf git libwrap0-dev libreadline-dev flex bison  libpcre2-dev libz-dev libzstd-dev bzip2 libpcap-dev libssh-dev liblinear-dev
 
-curl -L -o "${SOURCE_DIR}/${PACKAGE}-${VERSION}.${EXT}" "https://ftp.gnu.org/gnu/${PACKAGE}/${PACKAGE}-${VERSION}.${EXT}"
+curl -L -o "${SOURCE_DIR}/${PACKAGE}-${VERSION}.${EXT}" "https://ftp.gnu.org/gnu/wget/${PACKAGE}-${VERSION}.${EXT}"
 [ -d "${BUILD_DIR}/${PACKAGE}-${VERSION}" ] && rm -rf "${PACKAGE}-${VERSION}"
 cd "${BUILD_DIR}/" && tar -xf "${SOURCE_DIR}/${PACKAGE}-${VERSION}.${EXT}"
 rm -f "${SOURCE_DIR}/${PACKAGE}-${VERSION}.${EXT}"
