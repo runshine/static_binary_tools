b8d088c3250e6290c28b9487c1d6f74ebb272b91
runshine <447491275@qq.com>
2025-12-13 00:43:50 +0000

    fix nmap

--- build/nmap/build.sh
+++ build/nmap/build.sh
@@ -17,7 +17,6 @@ rm -f "${SOURCE_DIR}/${PACKAGE}-${VERSION}.${EXT}"
 cd "${BUILD_DIR}/${PACKAGE}-${VERSION}"
 CFLAGS=-static LDFLAGS="-static" ./configure --prefix=${INSTALL_DIR} --disable-shared --enable-static
 sed -i 's/ -llua5.4/ -llua5.4 -lm/g' Makefile
-sed -i 's///g' Makefile
 make install
 
 strip_elf_files "$INSTALL_DIR/sbin"
