59f17b0b225fc6c6518a595d5154e4dcb799a065
runshine <447491275@qq.com>
2025-12-13 09:25:22 +0000

    add vim build support

--- build/vim/build.sh
+++ build/vim/build.sh
@@ -9,7 +9,7 @@ PACKAGE="vim"
 EXT="tar.gz"
 apt-get -y install curl ca-certificates wget xz-utils libssl-dev libncurses-dev autoconf git libwrap0-dev libreadline-dev flex bison  libpcre2-dev libz-dev libzstd-dev bzip2 libpcap-dev libssh-dev liblinear-dev
 
-curl -o "${SOURCE_DIR}/${PACKAGE}-${VERSION}.${EXT}" "https://github.com/${PACKAGE}/${PACKAGE}/archive/refs/tags/v${VERSION}.${EXT}"
+curl -L -o "${SOURCE_DIR}/${PACKAGE}-${VERSION}.${EXT}" "https://github.com/${PACKAGE}/${PACKAGE}/archive/refs/tags/v${VERSION}.${EXT}"
 [ -d "${BUILD_DIR}/${PACKAGE}-${VERSION}" ] && rm -rf "${PACKAGE}-${VERSION}"
 cd "${BUILD_DIR}/" && tar -xf "${SOURCE_DIR}/${PACKAGE}-${VERSION}.${EXT}"
 rm -f "${SOURCE_DIR}/${PACKAGE}-${VERSION}.${EXT}"
