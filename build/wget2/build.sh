f4f2bc9d46ba71d7dc13fd663c9701458c6860b1
runshine <447491275@qq.com>
2025-12-13 09:43:31 +0000

    add wget2 binary

---
+++ build/wget2/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source "$(cd `dirname $0`;pwd)/../common/prepare_dir.sh"
+source "$(cd `dirname $0`;pwd)/../common/utils_func.sh"
+source "$(cd `dirname $0`;pwd)/../common/arch_detect.sh"
+
+VERSION="9.1.1975"
+PACKAGE="wget2"
+EXT="tar.gz"
+apt-get -y install curl ca-certificates wget xz-utils libssl-dev libncurses-dev autoconf git libwrap0-dev libreadline-dev flex bison  libpcre2-dev libz-dev libzstd-dev bzip2 libpcap-dev libssh-dev liblinear-dev
+
+curl -L -o "${SOURCE_DIR}/${PACKAGE}-${VERSION}.${EXT}" "https://ftp.gnu.org/gnu/${PACKAGE}/${PACKAGE}-${VERSION}.${EXT}"
+[ -d "${BUILD_DIR}/${PACKAGE}-${VERSION}" ] && rm -rf "${PACKAGE}-${VERSION}"
+cd "${BUILD_DIR}/" && tar -xf "${SOURCE_DIR}/${PACKAGE}-${VERSION}.${EXT}"
+rm -f "${SOURCE_DIR}/${PACKAGE}-${VERSION}.${EXT}"
+
+cd "${BUILD_DIR}/${PACKAGE}-${VERSION}"
+CFLAGS=--static LDFLAGS="--static" ./configure --prefix=${INSTALL_DIR}  --disable-shared --enable-static
+make install
+
+strip_elf_files "$INSTALL_DIR/sbin"
+strip_elf_files "$INSTALL_DIR/bin"
+package_release_tar "${INSTALL_DIR}" wget2-linux-${ARCH}.tar.gz
