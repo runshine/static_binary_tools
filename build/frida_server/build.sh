3c37430ab51359c0f05a8d15a69f7d8889c5fb81
runshine <447491275@qq.com>
2026-01-06 15:11:44 +0000

    add frida_server

--- build/frida_server/build.sh
+++ build/frida_server/build.sh
@@ -14,8 +14,8 @@ FRIDA_SERVER_X64="https://github.com/frida/frida/releases/download/17.5.2/frida-
 
 cd "$BUILD_DIR"
 curl -L -o "$BUILD_DIR/frida-server.xz" "${FRIDA_SERVER_AARCH64}"
-xz -d frida-server.xz && rm frida-server.xz
-mkdir -p $BUILD_DIR/bin/ && mv * bin/
+xz -d frida-server.xz
+mkdir -p $BUILD_DIR/bin/ && mv * bin/frida-server
 strip_elf_files "$BUILD_DIR/bin/"
 package_release_tar "${BUILD_DIR}" $INSTALL_DIR/frida_server-${VERSION}-linux-aarch64.tar.gz
 cd "$BUILD_DIR" && rm * -rf
@@ -23,8 +23,8 @@ cd "$BUILD_DIR" && rm * -rf
 
 cd "$BUILD_DIR"
 curl -L -o "$BUILD_DIR/frida-server.xz" "${FRIDA_SERVER_ARMHF}"
-xz -d frida-server.xz && rm frida-server.xz
-mkdir -p $BUILD_DIR/bin/ && mv * bin/
+xz -d frida-server.xz &&
+mkdir -p $BUILD_DIR/bin/ && mv * bin/frida-server
 strip_elf_files "$BUILD_DIR/bin/"
 package_release_tar "${BUILD_DIR}" $INSTALL_DIR/frida_server-${VERSION}-linux-armhf.tar.gz
 cd "$BUILD_DIR" && rm * -rf
@@ -32,8 +32,8 @@ cd "$BUILD_DIR" && rm * -rf
 
 cd "$BUILD_DIR"
 curl -L -o "$BUILD_DIR/frida-server.xz" "${FRIDA_SERVER_X64}"
-xz -d frida-server.xz && rm frida-server.xz
-mkdir -p $BUILD_DIR/bin/ && mv * bin/
+xz -d frida-server.xz
+mkdir -p $BUILD_DIR/bin/ && mv * bin/frida-server
 strip_elf_files "$BUILD_DIR/bin/"
 package_release_tar "${BUILD_DIR}" $INSTALL_DIR/frida_server-${VERSION}-linux-x86_64.tar.gz
 cd "$BUILD_DIR" && rm * -rf
