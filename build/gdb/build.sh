8e1a25fc35efaa6d6bb2321d8b40299d1b2b00c9
runshine <447491275@qq.com>
2026-01-07 14:02:07 +0000

    add gef support

--- build/gdb/build.sh
+++ build/gdb/build.sh
@@ -21,6 +21,7 @@ mkdir -p "$INSTALL_DIR/bin"
 cd "$INSTALL_DIR"
 curl -L -o "$INSTALL_DIR/bin/gdb.tar.gz" "${GDB_AARCH64}"
 cd "$INSTALL_DIR/bin" && tar -xf gdb.tar.gz && rm -rf gdb.tar.gz
+cp /build/gef.py "$INSTALL_DIR/bin/"
 strip_elf_files "$INSTALL_DIR/bin"
 package_release_tar "${INSTALL_DIR}" gdb-${VERSION}-linux-aarch64.tar.gz
 mv gdb-${VERSION}-linux-aarch64.tar.gz "$INSTALL_DIR/../"
@@ -29,6 +30,7 @@ mkdir -p "$INSTALL_DIR/bin"
 cd "$INSTALL_DIR"
 curl -L -o "$INSTALL_DIR/bin/gdb.tar.gz" "${GDB_ARMHF}"
 cd "$INSTALL_DIR/bin" && tar -xf gdb.tar.gz && rm -rf gdb.tar.gz
+cp /build/gef.py "$INSTALL_DIR/bin/"
 strip_elf_files "$INSTALL_DIR/bin"
 package_release_tar "${INSTALL_DIR}" gdb-${VERSION}-linux-armhf.tar.gz
 mv gdb-${VERSION}-linux-armhf.tar.gz "$INSTALL_DIR/../"
@@ -37,6 +39,7 @@ mkdir -p "$INSTALL_DIR/bin"
 cd "$INSTALL_DIR"
 curl -L -o "$INSTALL_DIR/bin/gdb.tar.gz" "${GDB_ARMEL}"
 cd "$INSTALL_DIR/bin" && tar -xf gdb.tar.gz && rm -rf gdb.tar.gz
+cp /build/gef.py "$INSTALL_DIR/bin/"
 strip_elf_files "$INSTALL_DIR/bin"
 package_release_tar "${INSTALL_DIR}" gdb-${VERSION}-linux-armel.tar.gz
 mv gdb-${VERSION}-linux-armel.tar.gz "$INSTALL_DIR/../"
@@ -45,6 +48,7 @@ mkdir -p "$INSTALL_DIR/bin"
 cd "$INSTALL_DIR"
 curl -L -o "$INSTALL_DIR/bin/gdb.tar.gz" "${GDB_X64}"
 cd "$INSTALL_DIR/bin" && tar -xf gdb.tar.gz && rm -rf gdb.tar.gz
+cp /build/gef.py "$INSTALL_DIR/bin/"
 strip_elf_files "$INSTALL_DIR/bin"
 package_release_tar "${INSTALL_DIR}" gdb-${VERSION}-linux-x86_64.tar.gz
 mv gdb-${VERSION}-linux-x86_64.tar.gz "$INSTALL_DIR/../"
