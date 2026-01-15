46ba8cecd6b128cca83440f96b4aa262f64cace9
runshine <447491275@qq.com>
2026-01-15 20:59:55 +0000

    update nacos package

--- build/nacos_client/build.sh
+++ build/nacos_client/build.sh
@@ -4,7 +4,7 @@ source "$(cd `dirname $0`;pwd)/../common/prepare_dir.sh"
 source "$(cd `dirname $0`;pwd)/../common/utils_func.sh"
 source "$(cd `dirname $0`;pwd)/../common/arch_detect.sh"
 
-apt update && apt-get -y install curl wget unzip nodejs npmnodejs npm
+apt update && apt-get -y install curl wget unzip nodejs npm
 
 VERSION="latest"
 
